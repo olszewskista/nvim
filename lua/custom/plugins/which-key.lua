a6658b8333b230317a826cb60e70fae9e4c96fab
olszewskista <olszewski.sta@gmail.com>
2025-06-10 00:02:42 +0200

    feat: Improve which-key config and add lazy-lock.json.

--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -1,18 +1,15 @@
 return {
   'folke/which-key.nvim',
   event = 'VeryLazy',
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-    {
-      '<leader>g',
-      function()
-        require('which-key').show { global = false }
-      end,
-      desc = 'Git: ',
-    },
-  },
+  config = function()
+    local wk = require 'which-key'
+
+    wk.add {
+      {
+        '<leader>g',
+        name = '[G]it',
+        icon = '',
+      },
+    }
+  end,
 }
