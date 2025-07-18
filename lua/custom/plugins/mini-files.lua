94f47ebc22b24c74c022bdbc14018aca168774cb
olszewskista <olszewski.sta@gmail.com>
2025-07-18 13:16:46 +0200

    feat: mini files preview

--- lua/custom/plugins/mini-files.lua
+++ lua/custom/plugins/mini-files.lua
@@ -32,5 +32,12 @@ return {
       desc = 'Open mini.files (cwd)',
     },
   },
-  config = true,
+  config = function()
+    require('mini.files').setup {
+      windows = {
+        preview = true,
+        width_preview = 50,
+      },
+    }
+  end,
 }
