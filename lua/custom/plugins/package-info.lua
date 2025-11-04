1b525f1e9b2564806004be0a906cac7d2b004274
Stanislaw Olszewski <stanislaw.olszewski@dynatrace.com>
2025-11-04 23:00:18 +0100

    remove package-info and hardtime

--- lua/custom/plugins/package-info.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'vuki656/package-info.nvim',
-  dependencies = { 'MunifTanjim/nui.nvim' },
-  config = function()
-    local pack = require 'package-info'
-    pack.setup()
-    vim.keymap.set('n', '<leader>nt', pack.toggle)
-  end,
-}
